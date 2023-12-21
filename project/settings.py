b0c6378ddcf0adb924854df17f7420d8c85cd370
Thales Plinio <thales_plinio@hotmail.com>
2023-12-21 18:07:08 -0400

    iniciando nosso site_setup no modo adm

--- project/settings.py
+++ project/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # My Apps
     'blog',
+    'site_setup',
 ]
 
 MIDDLEWARE = [
--- site_setup/models.py
+++ site_setup/models.py
@@ -7,8 +7,8 @@ class MenuLink(models.Model):
         verbose_name = "Menu Link"
         verbose_name = "Menu Links"
 
-    text = models.CharField(max_lenght=50)
-    url_or_path = models.CharField(max_lenght=2048)
+    text = models.CharField(max_length=50)
+    url_or_path = models.CharField(max_length=2048)
     new_tab = models.BooleanField(default=False)
 
     def __str__(self):
