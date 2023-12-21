741cad5e8a131cc40f0d914d1ff3020c0bc8ac32
Thales Plinio <thales_plinio@hotmail.com>
2023-12-21 18:29:05 -0400

    configurando nosso models e nosso admin do site_setup

--- site_setup/admin.py
+++ site_setup/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from site_setup.models import MenuLink
+from site_setup.models import MenuLink, SiteSetup
 
 
 # Register your models here.
@@ -8,3 +8,18 @@ class MenuLinkAdmin(admin.ModelAdmin):
     list_display = 'id', 'text', 'url_or_path',
     search_fields = 'id', 'text', 'url_or_path',
     list_display_links = 'id', 'text',
+
+
+class MenuLinkInLine(admin.TabularInline):
+    model = MenuLink
+    extra = 1
+
+# Register your models here.
+@admin.register(SiteSetup)
+class SiteSetupAdmin(admin.ModelAdmin):
+    list_display = 'title', 'description',
+    inlines = MenuLinkInLine,
+
+    # removendo a opção de adicionar mais de um setup
+    def has_add_permission(self, request):
+        return not SiteSetup.objects.exists()
