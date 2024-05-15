b5b74f63507a79a5ad66a50d864be3cf570c47d7
Thales Plinio <thales_plinio@hotmail.com>
2024-05-14 20:36:34 -0400

    removendo teste de context

--- site_setup/context_processors.py
+++ site_setup/context_processors.py
@@ -1,12 +1,6 @@
 from site_setup.models import SiteSetup
 
 
-def context_processor_example(request):
-    return {
-        'example': 'veio do context processor!!!!'
-    }
-
-
 def site_setup(request):
     setup = SiteSetup.objects.order_by("-id").first()
 
